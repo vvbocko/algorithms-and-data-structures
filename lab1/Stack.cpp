10b3f536a555f350b363a786b3e8ed6fe4b20704
vvbocko <wbocko05@gmail.com>
2026-03-07 17:25:51 +0100

    Additional Stack testing

--- lab1/Stack.cpp
+++ lab1/Stack.cpp
@@ -29,7 +29,7 @@ void Stack::Pop()
 {
     if(top == nullptr) 
     {
-        std::cout << "Stack is empty\n";
+        std::cout << "\nStack is empty";
         return;
     }
     Node* temp = top;
@@ -53,7 +53,7 @@ void Stack::Display()
 {
     if(top == nullptr) 
     {
-        std::cout << "Stack is empty\n";
+        std::cout << "\n Display stack:  Stack is empty\n";
         return;
     }
 
@@ -63,17 +63,28 @@ void Stack::Display()
         std::cout << current->data << ", ";
         current = current->next;
     }
-    std::cout << current->data;
+    std::cout << current->data << std::endl;
 }
 
 int main() 
 {
     Stack stack;
 
+    stack.Display();
+
+    std::cout << "Add 50 elements:\n";
     for(int i=0; i<50; i++)
     {
         stack.Push(i);
     }
+
     stack.Display();
+
+    std::cout << "Pop 51 element:\n";
+    for(int i=0; i<51; i++)
+    {
+        stack.Display();
+        stack.Pop();
+    }
 }
 
